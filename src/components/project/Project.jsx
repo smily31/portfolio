f78465434affaccbe0c229d7de1ee7cb09cff618
Ritika Bhatt <ritikabhatt3110@gmail.com>
2021-09-04 22:15:34 +0530

    project section added

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
 import './project.scss'
+import { data } from "../../data"
+import { useState } from "react";
 
 export default function Project() {
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const handleClick = (move) => {
+        move === "left"
+            ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : data.length - 1)
+            : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+    };
+
     return (
         <div className="project" id="project">
-            {/* project */}
+            <h1 className="title">My Personal Projects</h1>
+            <div className="slider" style={ {transform: `translateX(-${currentSlide * 100}vw)`}}>
+                {
+                    data.map( d => (
+                        <div className="container">
+                        <div className="item">
+                            <div className="left">
+                                <div className="info">
+                                <h1>{d.title}</h1>
+                                <h2>Description : </h2><p>{d.desc}</p>
+                                <h2>Web Stack : </h2>
+                                <ul>
+                                    {d.stack.map(i => <li>{i}</li>)}
+                                </ul>
+                                <h2>Github Repo : </h2><span><a href={d.github}>click me</a></span>
+                                <button type="button"><a href={d.project}>View Project</a></button>
+                                </div>
+                            </div>
+                            <div className="right">
+                                <div className="imageContainer">
+                                    <img src={d.image} alt="project" />
+                                </div>
+    
+                            </div>
+                        </div>
+                    </div>
+                    ))
+                }
+               
+            </div>
+                <img src="assest/leftArrow.png" alt="left Arrow" className="arrow left" onClick={()=>handleClick("left")} />
+                <img src="assest/rightArrow.png" alt="right Arrow" className="arrow right" onClick={()=>handleClick("right")} />
         </div>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const data = [
+    {
+        id : "1",
+        image : "assest/calculator.jpg",
+        title : "Calculator",
+        desc: "This is a simple web based attractive calculator",
+        stack: ["HTML" , "CSS" , "JAVASCIPT"],
+        github : "https://github.com/smily31/Calculator",
+        project : "https://smily31.github.io/Calculator/"
+    },
+    {
+        id : "2",
+        image : "assest/beatBox.png",
+        title : "Beat Box",
+        desc: "It plays sound when any above shown key is pressed",
+        stack: ["HTML" , "CSS" , "JAVASCIPT"],
+        github : "https://github.com/smily31/BeatBoxJS",
+        project : "https://smily31.github.io/BeatBoxJS/"
+    },
+    {
+        id : "3",
+        image : "assest/guessTheNo.png",
+        title : "Guess The Number",
+        desc: "It is a game which guesses your imagined number",
+        stack: ["HTML" , "CSS" , "JAVASCIPT"],
+        github : "https://github.com/smily31/guess-the-number",
+        project : "https://smily31.github.io/guess-the-number/"
+    },
+    {
+        id : "4",
+        image : "assest/responsiveCard.png",
+        title : "Birthday Invitation Card",
+        desc: "It is a attractive responsive birthday card",
+        stack: ["HTML" , "CSS" , "JAVASCIPT"],
+        github : "https://github.com/smily31/responsive-1",
+        project : "https://smily31.github.io/responsive-1/"
+    },
+    {
+        id : "5",
+        image : "assest/searchGif.png",
+        title : "GIF Search Engine",
+        desc: "This shows GIFs on your search",
+        stack: ["HTML" , "CSS" , "JAVASCIPT"],
+        github : "https://github.com/smily31/Gif-Search-Engine",
+        project : "https://smily31.github.io/Gif-Search-Engine/"
+    },
+    {
+        id : "6",
+        image : "assest/soundCloud.png",
+        title : "Sound Cloud Player",
+        desc: "Its a mini music player and it also shows your favorite show in side playlist",
+        stack: ["HTML" , "CSS" , "JAVASCIPT"],
+        github : "https://github.com/smily31/SoundCloud-Player",
+        project : "https://smily31.github.io/SoundCloud-Player/"
+    },
+    {
+        id : "7",
+        image : "assest/snakeGame.png",
+        title : "Snake-The Eater",
+        desc: "Its your forever snake game",
+        stack: ["HTML" , "CSS" , "JAVASCIPT"],
+        github : "https://github.com/smily31/Snake-The-eater",
+        project : "https://smily31.github.io/Snake-The-eater/"
+    },
+];
