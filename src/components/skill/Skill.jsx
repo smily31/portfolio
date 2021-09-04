55bb708d40827cebd1f8e7cc75483ae8e4b62835
Ritika Bhatt <ritikabhatt3110@gmail.com>
2021-09-05 03:00:57 +0530

    skill section added

--- src/components/skill/Skill.jsx
+++ src/components/skill/Skill.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './skill.scss'
+import { skill } from "../../skill"
 
 export default function Skill() {
     return (
-        <div className="skill" id="id">
-           {/* skill  */}
+        <div className="skill" id="skill">
+            <h2 className="title">
+                My Skills
+            </h2>
+           <div className="container">
+               {
+                   skill.map( s => (
+                       <div className="box">
+                           <img src={s.logo} alt="logo" />
+                       </div>
+                   )
+                   )
+               }
+           </div>
         </div>
     )
 }
---
+++ src/skill.js
@@ -0,0 +1,54 @@
+export const skill = [
+    {
+        id : "1",
+        logo : "assest/skillsLogo/cLogo.png"
+    },
+    {
+        id : "2",
+        logo : "assest/skillsLogo/c++Logo.png"
+    },
+    {
+        id : "3",
+        logo : "assest/skillsLogo/javaLogo.png"
+    },
+    {
+        id : "4",
+        logo : "assest/skillsLogo/htmlLogo.png"
+    },
+    {
+        id : "5",
+        logo : "assest/skillsLogo/cssLogo.png"
+    },
+    {
+        id : "6",
+        logo : "assest/skillsLogo/sassLogo.png"
+    },
+    {
+        id : "7",
+        logo : "assest/skillsLogo/bootstrapLogo.png"
+    },
+    {
+        id : "8",
+        logo : "assest/skillsLogo/reactLogo.png"
+    },
+    {
+        id : "9",
+        logo : "assest/skillsLogo/nodejsLogo.png"
+    },
+    {
+        id : "10",
+        logo : "assest/skillsLogo/mySqlLogo.png"
+    },
+    {
+        id : "11",
+        logo : "assest/skillsLogo/mongoLogo.png"
+    },
+    {
+        id : "12",
+        logo : "assest/skillsLogo/gitLogo.png"
+    },
+    {
+        id : "13",
+        logo : "assest/skillsLogo/linuxLogo.png"
+    },
+]
