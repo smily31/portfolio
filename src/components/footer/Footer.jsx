57306fbf5f433214921e24122e665df57f3e4b71
Ritika Bhatt <ritikabhatt3110@gmail.com>
2021-09-05 20:23:07 +0530

    footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,9 @@ import './footer.scss'
 export default function Footer() {
     return (
         <div className="footer" id="footer">
-          footer  
+          <div className="text">
+              Made with love during <a href="https://developdays.tech">Developers day</a> by <b>Ritika Bhatt</b> 
+          </div> 
         </div>
     )
 }
